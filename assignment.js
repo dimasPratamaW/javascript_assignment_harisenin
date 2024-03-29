2dc3a0fa6c260c8ed4a3f7d0162f69540ef9a110
dimasPratamaW <dimas.widyatmojo@gmail.com>
2024-03-30 01:53:27 +0700

    fix: benerin logic and finishing for checking the login info

--- assignment.js
+++ assignment.js
@@ -1,6 +1,6 @@
-async function getListUserJson() {
+async function getListUserJson() { // menarik data dari json
   try {
-    const response = await fetch("./listUser.json");
+    const response = await fetch("listUser.json");
     if (!response.ok) {
       throw new Error("Failed to fetch");
     }
@@ -15,20 +15,23 @@ async function getListUserJson() {
 const title = document.getElementById("title");
 const rememberPass = document.getElementById("rememberPass");
 
-function onClick() {
+async function onClick() {
   const getEmail = document.getElementById("email");
   const getPass = document.getElementById("password");
 
   const email = getEmail.value;
   const password = getPass.value;
 
-  checkUser(email, password);
+  await checkUser(email, password);
 }
 
-function checkUser(email, password) {
+async function checkUser(email, password) {
+  var rememberPassDetail = ""
+  showRemember(rememberPassDetail)
+
   getListUserJson().then((listUser) => {
     console.log(listUser.length);
-    let counter = 0;
+    let counter = -1;
     for (let i = 0; i < listUser.length; i++) {
       if (listUser[i].email === email) {
         counter = i
@@ -36,10 +39,28 @@ function checkUser(email, password) {
     }
     console.log(counter,"ini index of the data")
 
-    if (counter !== 0) {
+    if (counter === -1){
+      return alert("the email didnt exist, please tell admin for register your email")
+    }
+
+    if (counter > -1) {
       // check pass in this area
-      const detailUser = listUser[i];
-      console.log(detailUser);
+      console.log("User Password:", listUser[counter].pass);
+      console.log("password yang dimasuki",password);
+
+      if(listUser[counter].pass === password){
+        rememberPassDetail = `You're Login`
+        return showRemember(rememberPassDetail)
+      }
+      if(listUser[counter].pass !== password){
+        rememberPassDetail = `${listUser[counter].rememberPass}`
+        return showRemember(rememberPassDetail)
+        
+      }
     }
   });
+}
+
+function showRemember(passDetail){
+  rememberPass.innerHTML = passDetail
 }
